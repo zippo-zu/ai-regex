0255b608ff9266171ff04b10718a2270e43de23f
zupo@smcaiot.com <zupo@smcaiot.com>
2023-09-05 22:37:55 +0800

    init project ai regex

---
+++ README.md
@@ -0,0 +1,20 @@
+# Regex AI App
+
+React and Node based App, using OpenAI's api to provide Regex generation.
+
+## How to Use
+
+In the project directory, open terminal and install dependencies by using following command:
+
+### `npm install`
+
+## Config
+You need to put your own OpenAI api key in the ".env" file. Sorry, it's not up to me, OpenAI enforces this.
+
+### Run Application
+
+Use following command:
+
+### `npm run dev`
+
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ src/server.js
@@ -0,0 +1,43 @@
+// Import dependencies and middlewares 
+
+const express = require("express");
+require('dotenv').config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Configuration, OpenAIApi } = require("openai");
+
+// Set up the server ////////////
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+// Set up OpenAI endpoint
+
+const configuration = new Configuration({
+  apiKey: process.env.CHATBOT_KEY,basePath:process.env.BASE_URL
+});
+// CHATBOT_KEY is key name in .env file. .env file should be in project root directory - format is below
+// CHATBOT_KEY="YOR-API-KEY"
+
+const openai = new OpenAIApi(configuration);
+app.post("/v1/completions", async (req, res) => {
+  const { sampleText, target } = req.body;
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003", 
+    prompt: `Write a Regex expression for the following text "${sampleText}"
+      to being able to find the following: "${target}". Response only with a working expression.
+    `,
+    max_tokens: 2048,
+  });
+  res.send(completion.data.choices[0].text);
+});
+// 'prompt' is coming from axios post - from react js state - its input field value or query or question 
+
+// Start the server ////////////////////
+
+const port = 5555;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
